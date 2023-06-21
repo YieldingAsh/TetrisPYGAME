d4188e37011a0186d0a72e539138ac5e11c4a169
YieldingAsh <99512390+YieldingAsh@users.noreply.github.com>
2023-06-21 09:40:22 -0300

    Add files via upload

---
+++ tetrispygame/settings.py
@@ -0,0 +1,33 @@
+import pygame
+
+vec = pygame.math.Vector2
+
+FPS = 60
+FIELD_COLOR = ('black')
+BG_COLOR = ('black')
+FONT_PATH = ('tetrispygame/Font/ComicSansMS3.ttf')
+SPRITE_PATH =('tetrispygame/Sprites')
+
+ANIM_TIME_INTERVALE = 150
+FAST_ANIM_TIME_INTERVALE = 15
+
+TILE_SIZE = 50
+FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
+FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE 
+
+FIELD_SCALE_W,FIELD_SCALE_H = 1.7, 1.0
+WIN_RES = WIN_W, WIN_H = FIELD_RES[0] * FIELD_SCALE_W, FIELD_RES[1] * FIELD_SCALE_H
+
+INIT_POS_OOFSET = vec(FIELD_W // 2 - 1, 0)
+NEXT_POS_OFFSET = vec(FIELD_W * 1.3, FIELD_H * 0.45)
+MOVE_DIRECTIONS = {'left': vec(-1, 0), 'right': vec(1, 0), 'down': vec(0, 0.5)}
+
+TETROMINOES = {
+    'T': [(0, 0), (-1, 0), (1,0), (0, -1)],
+    'O': [(0, 0), (0, -1), (1, 0), (1,-1)],
+    'J': [(0, 0), (-1, 0), (0, -1), (0, -2)],
+    'L': [(0, 0), (1, 0), (0, -1), (0, -2)],
+    'I': [(0, 0), (0, 1), (0, -1), (0, -2)],
+    'S': [(0, 0), (-1, 0), (0, -1), (1, -1)],
+    'Z': [(0, 0), (1, 0), (0, -1), (-1, -1)]
+}
